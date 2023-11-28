498309e2ee4b859a958ba3c20dfc48e77709f32d
Daniel <danielsantos.senai564@gmail.com>
2023-11-28 16:59:30 -0300

    add footer

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import Styles from './footer.module.css';
 
 const Footer = () => {
   const footerStyle = {
-    backgroundColor: '#6A0572', // roxo
-    color: '#000000', // preto
+    backgroundColor: '#000000', // roxo
+    color: '#6A0572', // preto
     padding: '20px',
     textAlign: 'center',
   };
 
   return (
     <footer style={footerStyle}>
-      <p>Seu conteúdo do footer aqui</p>
+      <p>2023
+        Five Nights at Freddy's
+        . Todos os direitos reservados.©</p>
+        <div id={Styles.icons}>
+      <div><FaInstagram /></div>
+      <div><FaFacebook /></div>
+      <div><FaXTwitter /></div>
+      <div><IoLogoYoutube /></div>
+      <div><FaGithub /></div>
+      </div>
     </footer>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Footer from './components/footer/Footer'
+
 
 export default function Home() {
   return (
     <main>
-      
+      <Footer />
     </main>
   )
 }
