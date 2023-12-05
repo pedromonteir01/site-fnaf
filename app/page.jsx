ac3e8f72ab1d7d0fb10355e65b84a344af98ad3b
João Pedro Rocha <jpcrocha8@gmail.com>
2023-12-05 10:57:04 -0300

    adjust home

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
 'use client'
 import styles from './page.module.css';
 import SideHeader from './components/header/Header';
-//import Footer from './components/footer/Footer';
+// import Footer from './components/footer/Footer';
 
 const Home = () => {
+
   return (
-    <div>
-    <main id={styles.main}>
+    <div id={styles.main}>
+      <h1 className={styles.titlePage}>FIVE NIGHTS AT FREDDY'S</h1>
+      <div className={styles.header}>
       <SideHeader />
+      </div>
       <div className={styles.main2}>
-        <div className={styles.divTitle}>
-        <h1 className={styles.titlePage}>FIVE NIGHTS AT FREDDY'S</h1>
+        <div className={styles.main3}>
+          <p>ojsdviyhbwshivbh i</p>
         </div>
       </div>
-    </main>
-    {/* <Footer /> */}
+      {/* <Footer /> */}
     </div>
   )
 }
